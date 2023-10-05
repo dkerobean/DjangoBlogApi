190ff769d8526277d5c2a31ec85f03bc71ff7ce8
Dickson <dkerobean@gmail.com>
2023-10-05 12:28:36 +0000

    contact create view done

---
+++ blog/contact/tests/test_contact.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from contact.models import Contact
+
+
+class ContactTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.contact_data = {
+            'name': 'Test Name',
+            'email': 'email@example.com',
+            'message': 'Test Message'
+        }
+
+    def test_contact_create(self):
+        self.url = reverse('contact-create')
+        response = self.client.post(self.url, self.contact_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(Contact.objects.count(), 1)
+        self.assertTrue(Contact.objects.get().name, 'Test Name')
