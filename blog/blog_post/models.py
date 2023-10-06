0a07ab303fe95e233a3e312a5b190b159490531d
Dickson <dkerobean@gmail.com>
2023-10-06 13:55:28 +0000

    models done

--- blog/blog_post/models.py
+++ blog/blog_post/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from user.models import CustomUser
 
-# Create your models here.
+
+# UserProfile model
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    position = models.CharField(max_length=150)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    website = models.URLField(blank=True)
+    github = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='blog_pics/', blank=True)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.user.username} on {self.post.title}"
--- blog/contact/urls.py
+++ blog/contact/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('view/', views.ContactListView.as_view(), name="contact-view"),
     path('create/', views.ContactListView.as_view(), name="contact-create"),
-    path('get/<int:pk>/', views.ContactDetailView.as_view(), name="contact-detail"),
-    path('delete/<int:pk>/', views.ContactDetailView.as_view(), name="contact-delete"),
+    path('get/<int:pk>/', views.ContactDetailView.as_view(), name="contact-detail"), # noqa
+    path('delete/<int:pk>/', views.ContactDetailView.as_view(), name="contact-delete"), # noqa
 ]
